80b16ef1157c0205b2d90e81fdafb41b46e10b97
M <markpackham1@gmail.com>
2025-08-07 16:33:01 +0100

    Add noscript to the old default behavior
    https://youtu.be/ryint_sNZoU?t=120

--- resources/views/outline/index.blade.php
+++ resources/views/outline/index.blade.php
@@ -2,18 +2,21 @@
 
 @section('content')
   <div class="outline-wrapper">
-    <h1 class="page-title">Outline Home</h1>
 
+
+    <noscript>
+    <h1 class="page-title">Outline Home</h1>
     <div class="content">
       <h2 class="text-bold text-center text-4xl my-12">Start Outlining Your Novel!</h2>
       <p class="text-center my-12 max-w-1/2 mx-auto">
-        JavaScript is recommended for a better experience.
+      JavaScript is recommended for a better experience.
       </p>
       <div class="flex justify-center my-8 gap-8 max-w-1/2 mx-auto">
-        <a href="{{ route('outline.chapters.index') }}" class="btn">Chapter Timeline</a>
-        <a href="{{ route('outline.codex.index') }}" class="btn">Codex Entries</a>
+      <a href="{{ route('outline.chapters.index') }}" class="btn">Chapter Timeline</a>
+      <a href="{{ route('outline.codex.index') }}" class="btn">Codex Entries</a>
       </div>
     </div>
-    
+    </noscript>
+
   </div>
 @endsection
